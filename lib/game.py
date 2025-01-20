b0fa85260c0d06ae96c34572e5219d3c4fb1a321
Thomas Sigler <ts309435@gmail.com>
2025-01-19 18:23:54 -0600

    Initial Screen Work

---
+++ lib/game.py
@@ -0,0 +1,38 @@
+import pygame as pg
+import sys
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((640, 480))
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.glob_event = pg.USEREVENT
+        self.glob_tigger = False
+        pg.time.set_timer(self.glob_event, 40)
+        self._end = False
+
+    def check_events(self):
+        self.glob_trigger = False
+        for e in pg.event.get():
+            if e.type == pg.QUIT or (e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+            elif e.type == self.glob_event:
+                self.glob_trigger = True
+
+    def update(self):
+        pg.display.flip()
+        self.delta_tim = self.clock.tick()
+        pg.display.set_caption(f'GGJ PyGame Game')
+
+
+    def run(self):
+        while not self._end:
+            self.check_events()
+            self.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
