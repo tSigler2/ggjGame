1fbeb011e783770968468ad68dc2947c5f35ce39
David Jones <86750742+phantom-opera@users.noreply.github.com>
2025-01-24 19:04:18 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAA
    AAA

--- GameLib/main.py
+++ GameLib/main.py
@@ -4,5 +4,5 @@ from GameLib.menu import Menu
 
 if __name__ == "__main__":
     # window dimensions and start
-    menu = Menu((800, 600), enable_test_level=True)  # Width: 800px, Height: 600px
+    menu = Menu((800, 600), enable_test_level=False)  # Width: 800px, Height: 600px
     menu.run()
