fd5446b2020df9fcf5fd8bfe5355c1bfbcb3f9e6
Spade <47337405+SagarPateI@users.noreply.github.com>
2025-01-25 06:50:09 -0500

    Update Player.py

--- test_idea/Player.py
+++ test_idea/Player.py
@@ -2,8 +2,11 @@ from collections import deque
 import os
 import pygame as pg
 
+
 class Player:
-    def __init__(self, game, init_sprite, animation_path, pos, animation_time, coords, *args):
+    def __init__(
+        self, game, init_sprite, animation_path, pos, animation_time, coords, *args
+    ):
         self.game = game
         self.sprite = pg.image.load(init_sprite).convert_alpha()
 
@@ -35,22 +38,58 @@ class Player:
         keys = pg.key.get_pressed()
         curr_time = pg.time.get_ticks()
 
-        if keys[pg.K_w] and self.coords[1] > 0 and (curr_time - self.prev_move_time) >= self.delta_move:
+        if (
+            keys[pg.K_w]
+            and self.coords[1] > 0
+            and (curr_time - self.prev_move_time) >= self.delta_move
+        ):
             self.prev_move_time = curr_time
             self.coords[1] -= 1
-            self.move((self.game.map[self.coords[0]][self.coords[1]].x, self.game.map[self.coords[0]][self.coords[1]].y))
-        elif keys[pg.K_s] and self.coords[1] < 10 and (curr_time - self.prev_move_time) >= self.delta_move:
+            self.move(
+                (
+                    self.game.map[self.coords[0]][self.coords[1]].x,
+                    self.game.map[self.coords[0]][self.coords[1]].y,
+                )
+            )
+        elif (
+            keys[pg.K_s]
+            and self.coords[1] < 10
+            and (curr_time - self.prev_move_time) >= self.delta_move
+        ):
             self.prev_move_time = curr_time
             self.coords[1] += 1
-            self.move((self.game.map[self.coords[0]][self.coords[1]].x, self.game.map[self.coords[0]][self.coords[1]].y))
-        elif keys[pg.K_d] and self.coords[0] < 10 and (curr_time - self.prev_move_time) >= self.delta_move:
+            self.move(
+                (
+                    self.game.map[self.coords[0]][self.coords[1]].x,
+                    self.game.map[self.coords[0]][self.coords[1]].y,
+                )
+            )
+        elif (
+            keys[pg.K_d]
+            and self.coords[0] < 10
+            and (curr_time - self.prev_move_time) >= self.delta_move
+        ):
             self.prev_move_time = curr_time
             self.coords[0] += 1
-            self.move((self.game.map[self.coords[0]][self.coords[1]].x, self.game.map[self.coords[0]][self.coords[1]].y))
-        elif keys[pg.K_a] and self.coords[0] > 0 and (curr_time - self.prev_move_time) >= self.delta_move:
+            self.move(
+                (
+                    self.game.map[self.coords[0]][self.coords[1]].x,
+                    self.game.map[self.coords[0]][self.coords[1]].y,
+                )
+            )
+        elif (
+            keys[pg.K_a]
+            and self.coords[0] > 0
+            and (curr_time - self.prev_move_time) >= self.delta_move
+        ):
             self.prev_move_time = curr_time
             self.coords[0] -= 1
-            self.move((self.game.map[self.coords[0]][self.coords[1]].x, self.game.map[self.coords[0]][self.coords[1]].y))
+            self.move(
+                (
+                    self.game.map[self.coords[0]][self.coords[1]].x,
+                    self.game.map[self.coords[0]][self.coords[1]].y,
+                )
+            )
 
     def move(self, val):
         self.x, self.y = val
@@ -58,7 +97,7 @@ class Player:
     def dump_animations(self, path, *args):
         for k in args[0]:
             self.anim_paths[k] = deque()
-            for img in sorted(os.listdir(path+"/"+k)):
+            for img in sorted(os.listdir(path + "/" + k)):
                 self.anim_paths[k].append(pg.image.load(path + "/" + k + "/" + img))
 
     def update(self):
